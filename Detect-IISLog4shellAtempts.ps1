968084060875d4a2995e71e1df65a443b2c2572e
David Just <57944742+djust270@users.noreply.github.com>
2021-12-14 11:30:05 -0500

    Create Detect-IISLog4shellAtempts.ps1

---
+++ Detect-IISLog4shellAtempts.ps1
@@ -0,0 +1,4 @@
+# Parse IIS logs for Log4Shell attempts
+# Logs will be dumped to C:\temp\log4shellAttempts.txt
+mkdir C:\temp
+gci "C:\inetpub\logs\LogFiles\W3SVC1" -file -recurse | foreach {Get-Content $_.fullname | select-string -simplematch "jndi" | out-file C:\temp\log4shellAttempts.txt}
