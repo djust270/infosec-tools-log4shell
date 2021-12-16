12e4e68cb08444aea6f69d29884838b7f232d385
David Just <57944742+djust270@users.noreply.github.com>
2021-12-16 07:06:08 -0500

    Update Detect-IISLog4shellAtempts.ps1

--- Detect-IISLog4shellAtempts.ps1
+++ Detect-IISLog4shellAtempts.ps1
@@ -1,8 +1,14 @@
 # Parse IIS logs for Log4Shell attempts
 # Logs will be dumped to C:\temp\log4shellAttempts.txt
 mkdir C:\temp
-gci "C:\inetpub\logs\LogFiles\W3SVC1" -file -recurse | foreach {Get-Content $_.fullname | select-string -simplematch "jndi" | out-file C:\temp\log4shellAttempts.txt -append}
-gci "C:\inetpub\logs\LogFiles\W3SVC1" -file -recurse | foreach {Get-Content $_.fullname | select-string -simplematch "lower" | out-file C:\temp\log4shellAttempts.txt -append}
-gci "C:\inetpub\logs\LogFiles\W3SVC1" -file -recurse | foreach {Get-Content $_.fullname | select-string -simplematch '${$::' | out-file C:\temp\log4shellAttempts.txt -append}
-gci "C:\inetpub\logs\LogFiles\W3SVC1" -file -recurse | foreach {Get-Content $_.fullname  | select-string -simplematch 'upper' | out-file C:\temp\log4shellAttempts.txt -append
+$logs = gci "C:\inetpub\logs\LogFiles\W3SVC1" -file -recurse 
+$content = $logs | foreach {get-content $_.fullname}
+$content | select-string -simplematch "jndi" | out-file C:\temp\log4shellAttempts.txt -append
+$content | select-string -simplematch 'lower' | out-file C:\temp\log4shellAttempts.txt -append
+$content | select-string -simplematch 'upper' | out-file C:\temp\log4shellAttempts.txt -append
+$content | select-string -simplematch '${' | out-file C:\temp\log4shellAttempts.txt -append
 & "C:\temp\log4shellAttempts.txt"
+
+
+
+ 
