3ef3050f503407d42370a26e71f41e5032376c8f
David Just <57944742+djust270@users.noreply.github.com>
2021-12-20 19:01:26 -0500

    Add files via upload

---
+++ log4jdetect.py
@@ -0,0 +1,50 @@
+from logging import exception
+import os
+import fnmatch
+import zipfile
+import json
+import socket
+
+path = '/'
+
+jarfiles = []
+jndimatch = []
+jarfiles.extend([os.path.join(dirpath, f)
+    for dirpath, dirnames, files in os.walk(path)
+    for f in fnmatch.filter(files, '*.jar')])
+
+def iter_jarfile(fobj, parents=None, stats=None):
+    """
+    Yields (zfile, zinfo, zpath, parents) for each file in zipfile that matches `FILENAMES` or `JAR_EXTENSIONS` (recursively)
+    """
+    parents = parents or []
+    
+    try :
+        with zipfile.ZipFile(fobj) as zfile:
+            for zinfo in zfile.infolist():
+                if fnmatch.fnmatch((zinfo.filename),"*JNDILookup.class"):
+                    jndimatch.append(fobj)
+    except:
+        print(exception)
+
+def set_default(obj):
+    if isinstance(obj, set):
+        return list(obj)
+    raise TypeError                
+
+for f in jarfiles:
+    iter_jarfile(f)
+
+if jndimatch:
+    hostname=socket.gethostname()
+    domain=socket.getfqdn()
+    stringJNDI = str(jndimatch)
+    vulnfiles = stringJNDI
+    filecount = len(jndimatch)
+    print('WARNING: ', filecount , 'potentially vulnerable files detected')
+    print('Vulnerable Files:' , vulnfiles)
+
+else:
+    print('No vulnerable files detected')
+
+
